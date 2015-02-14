d5a9e186f4f9887de2b2ef8e4194f53e36361cd2
wea_ondara <wea_ondara@alpenblock.net>
2015-02-14 14:32:30 +0100

    meh

--- exceptions/IndexOutOfBoundsException.cpp
+++ exceptions/IndexOutOfBoundsException.cpp
@@ -5,10 +5,14 @@
  * Created on 14. Februar 2015, 12:03
  */
 
+#include <string>
+
 #include "IndexOutOfBoundsException.h"
 
+using std::string;
+
 IndexOutOfBoundsException::IndexOutOfBoundsException(size_t index, size_t max)
-: Exception("index was " + index + "but size was " + max), index(index), max(max)
+: Exception((char*)(string("index was ") + std::to_string(index) + string("but size was ") + std::to_string(max)).c_str()), index(index), max(max)
 {
 }
 
